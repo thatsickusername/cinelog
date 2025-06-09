b687617a52c4d82bbf62d68128f2a3f53e172c6f
thatsickusername <manish1234pandeycrpf@gmail.com>
2025-06-09 13:52:22 +0530

    firebase authentication with google set up

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import { Link, useNavigate } from 'react-router-dom'
 import './Navbar.css'
 import { useState } from 'react'
+import { useAuth } from '../../context/useAuth'
 
 function Navbar(){
+    const {user, signInWithGoogle, logout} = useAuth()
+
+    const [dropdownOpen, setDropdownOpen] = useState(false)
     const [searchValue, setSearchValue] = useState("")
     const navigate = useNavigate();
 
@@ -10,6 +14,23 @@ function Navbar(){
         e.preventDefault();
         console.log(searchValue)
         navigate(`/results?search_query=${encodeURIComponent(searchValue)}`)
+    }
+
+    const handleGoogleLogin = async () =>{
+        try{
+            await signInWithGoogle();
+        } catch (error) {
+            console.log('error: ', error)
+        }
+    }
+
+    function handleLogout (){
+        logout()
+        setDropdownOpen(prevState => !prevState)
+    }
+
+    function handleDropdownToggle(){
+        setDropdownOpen(prevState => !prevState)
     }
 
     return(
@@ -25,6 +46,28 @@ function Navbar(){
                         onChange={(e)=> setSearchValue(e.target.value)}>
                 </input>
             </form>
+            <div className='userLinks'>
+                {user && (
+                    <div className='userButton' onClick={handleDropdownToggle}>
+                    {user?.displayName}
+                    <span className={`dropdownArrow ${dropdownOpen ? 'open' : ''}`}>▼</span>
+                    </div>
+                )}
+
+                {dropdownOpen && (
+                    <div className='userDropdown'>
+                    <div>Your Profile</div>
+                    <div onClick={handleLogout}>Log Out</div>
+                    </div>
+                )}
+
+                {!user && (
+                    <div className='userButton' onClick={handleGoogleLogin}>
+                    Log In
+                    </div>
+                )}
+            </div>
+
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import HomePage from './pages/HomePage/HomePage.jsx'
 import './main.css'
 import Movie from './pages/Movie/Movie.jsx'
 import SearchResultsPage from './pages/SearchResultsPage/SearchResultsPage.jsx'
+import { AuthProvider } from './context/authProvider.jsx'
 
 const router = createBrowserRouter([
   {
@@ -30,6 +31,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <AuthProvider>
+      <RouterProvider router={router}/>
+    </AuthProvider>
   </StrictMode>,
 )
